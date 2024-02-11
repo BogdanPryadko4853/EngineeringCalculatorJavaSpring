699cedab5a148b667af6f984cd7695ad5bae889c
PryadkoBogdan <bogdanpryadko1@gmail.com>
2024-02-11 14:51:07 +0300

    first

---
+++ src/main/java/org/engineeringcalculatorjavaspring/engineeringcalculatorjavaspring/EngineeringCalculatorJavaSpringApplication.java
@@ -0,0 +1,13 @@
+package org.engineeringcalculatorjavaspring.engineeringcalculatorjavaspring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EngineeringCalculatorJavaSpringApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(EngineeringCalculatorJavaSpringApplication.class, args);
+    }
+
+}
---
+++ src/test/java/org/engineeringcalculatorjavaspring/engineeringcalculatorjavaspring/EngineeringCalculatorJavaSpringApplicationTests.java
@@ -0,0 +1,13 @@
+package org.engineeringcalculatorjavaspring.engineeringcalculatorjavaspring;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EngineeringCalculatorJavaSpringApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
